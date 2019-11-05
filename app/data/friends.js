a9ced30cde87ee8a312f2d5e20c1ce449305750b
Kate Laney <kate.a.laney@gmail.com>
2019-11-05 13:50:11 -0500

    updating html modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,26 +1,26 @@
 var friends = [
     {
-      friendName: "Granny Weatherwax",
+      name: "Granny Weatherwax",
       photo: "https://saucydryad.files.wordpress.com/2018/04/granny.jpg",
       scores: [1, 2, 4, 3, 1, 5, 1, 1, 3, 5]
     },
     {
-      friendName: "Nanny Ogg",
+      name: "Nanny Ogg",
       photo: "https://assets.mycast.io/characters/121124_normal.jpg?1548271799",
       scores: [5, 5, 5, 4, 1, 1, 1, 1, 2, 2]
     },
     {
-      friendName: "Sam Vimes",
+      name: "Sam Vimes",
       photo: "https://i.pinimg.com/474x/0f/4f/e5/0f4fe5c2aa8023704e9f778ed64c1204--terry-pratchett-discworld-sam-sam.jpg",
       scores: [1, 2, 4, 3, 3, 4, 1, 3, 4, 4]
     },
     {
-      friendName: "Carrot Ironfoundersson",
+      name: "Carrot Ironfoundersson",
       photo: "https://vignette.wikia.nocookie.net/discworld/images/0/04/Carrot-Ironfoundersson-1.jpg/revision/latest/scale-to-width-down/266?cb=20190228222706",
       scores: [2, 5, 2, 5, 1, 2, 1, 1, 1, 5]
     },
     {
-      friendName: "Rincewind",
+      name: "Rincewind",
       photo: "https://vignette.wikia.nocookie.net/discworld/images/e/e8/Rincewind.jpg/revision/latest/scale-to-width-down/276?cb=20150514182916",
       scores: [3, 3, 4, 3, 5, 2, 5, 4, 1, 2]
     },
